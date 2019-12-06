d231653ea9a1cf29de7cd33697d2e01d0dfb8ffe
jacklynmendoza61 <58575729+jacklynmendoza61@users.noreply.github.com>
2019-12-06 09:16:01 +0800

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -11,18 +11,16 @@ public class Main1 {
         System.out.println("hello world");   
         Scanner obj = new Scanner (System.in);
                   
-                   
-               
-               System.out.println("What is your name?");
-                  String name = obj.nextLine();
-               System.out.println("How old are you?");
-                  String age = obj.nextLine();
-               System.out.println("What's your address?");
-                  String add = obj.nextLine();
+        System.out.println("What is your name?");
+        String name = obj.nextLine();
+        System.out.println("How old are you?");
+        String age = obj.nextLine();
+        System.out.println("What's your address?");
+        String add = obj.nextLine();
                   
-               System.out.println("hello" + name);
-               System.out.println("How old" + age);
-               System.out.println("Address" + add); 
+        System.out.println("hello" + name);
+        System.out.println("How old" + age);
+        System.out.println("Address" + add); 
     }
     
 }
