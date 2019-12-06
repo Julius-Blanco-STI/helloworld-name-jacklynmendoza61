5c27f50f8f4e4683da8b81fa4ce88700bfa2ca5b
jacklynmendoza61 <58575729+jacklynmendoza61@users.noreply.github.com>
2019-12-06 08:37:24 +0800

    Add files via upload

---
+++ MainJava.java
@@ -0,0 +1,14 @@
+
+package main.java;
+
+
+public class MainJava {
+
+    
+    public static void main(String[] args) {
+            String name ="jacklyn";
+               
+               System.out.println("hello world" + name);
+    
+}
+}
